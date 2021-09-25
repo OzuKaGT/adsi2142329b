cb75f40b3177fb70284cb2baffa5070f0e107cae
OzuKaGT <oaamazo@gmail.com>
2021-09-25 11:39:39 -0500

    Version 1 del Portafolio

---
+++ PORTAFOLIO/Vista/Index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Estilo/Style.css">
+    <title> Andres Amazo </title>
+</head>
+<body>
+    <header>
+        <h1> Andres Amazo </h1>
+        <h3> Developer Portfolio </h3>
+        <nav>
+            <ul>
+                <li> Acerca de mi </li>
+                <li> Manuales </li>
+                <li> Proyectos en JS </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <aside>
+                <p> Hola ! 
+                    Soy Andres Amazo, un aprendiz de desarrollo de paginas web
+                </p>
+            </aside>
+            <aside>
+                <p> Este es mi portafolio de desarrollador, en el cual te mostrare los trabajos que he realizado</p>
+            </aside>
+        </section>
+    </main>
+</body>
+</html>
