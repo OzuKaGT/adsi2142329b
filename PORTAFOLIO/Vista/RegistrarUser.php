a4c59aa56cf3e543f70ea9d6cc0b1f1ad25e9e84
ozuka <oaamazo@misena.edu.co>
2021-10-03 19:12:38 -0500

    Version 2 Portafolio

--- PORTAFOLIO/Vista/RegistrarUser.php
+++ PORTAFOLIO/Vista/RegistrarUser.php
@@ -4,42 +4,72 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Estilo/Style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <title> Andres Amazo </title>
 </head>
 <body>
-    <header>
-        <h1> Andres Amazo </h1>
-        <h3> Developer Portfolio </h3>
-        <nav>
-            <ul>
-                <li> Acerca de mi </li>
-                <li> Manuales </li>
-                <li> Proyectos en JS </li>
-            </ul>
-        </nav>
-    </header>
-    <main>
-        <section>
-            <form action="" method='POST'>
-                <legend> Nombres </legend>
-                <input type="text" name="name" placeholder="Nombres" required><br><br>
-                <legend> Apellidos </legend>
-                <input type="text" name="lname" placeholder="Apellidos" required><br><br>
-                <legend> Tipo de Documento </legend>
-                <select name="tdocumento" required>
-                    <option value="">------Seleccione------</option>
-                    <option value="CC"> Cedula de Ciudadania </option>
-                    <option value="TI"> Tarjeta de Identidad </option>
-                    <option value="CE"> Cedula de Extrangeria </option>
-                    <option value="Pass"> Pasaporte </option>
-                </select><br><br>
-                <legend> Numero de Documento </legend>
-                <input type="text" name="doc" placeholder="Numero de Documento" required><br><br>
-                <legend> Correo Electronico </legend>
-                <input type="text" name="email" placeholder="Correo Electronico" required><br><br>
-                <input type="submit" value="Registrar">
-            </form>
+    <br>
+    <div class="container-lg fw-bolder" style="background-color:#9A71FF;border-radius:8px;">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light0" style="background-color:#9A71FF;">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+      <img src="Imagenes/Passione.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      Andres Amazo
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Proyectos</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" >
+            Manuales
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color:#9A71FF;"> 
+            <li><a class="dropdown-item" href="#" style="background-color:#9A71FF;">Manual de Usuario</a></li>
+            <li><a class="dropdown-item" href="#" style="background-color:#9A71FF;">Manual de Sistema</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Usuarios" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </div>
+    <br>
+    <div class="container-lg" style="background-color: #9A71FF; border-radius:8px;"><br>
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nombres</label>
+                <input type="text" class="form-control" id="exampleInputEmail1">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form><br>
+    </div>
         </section>
     </main>
 </body>
